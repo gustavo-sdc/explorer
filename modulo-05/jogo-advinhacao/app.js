fb396fbf58ff0d54b91f7768298cf61e9df6ec73
Gustavo Santos de Campos <110877527+GustavoSdC-00@users.noreply.github.com>
2023-12-19 06:48:42 -0300

    Jogo de adivinhação

--- modulo-05/jogo-advinhacao/app.js
+++ modulo-05/jogo-advinhacao/app.js
@@ -1,25 +1,53 @@
 // Variables
 
-let randomNumber, attempt, response
+let randomNumber, attempt, response, attemptNumber
 const buttonSend = document.querySelector("#buttonSend")
+const buttonAgain = document.querySelector("#again")
+const attemptBox = document.querySelector("#attemptBox")
+const responseBox = document.querySelector("#responseBox")
+const responseContent = document.querySelector("#responseContent")
+
+attemptNumber = 0 
+attempt = document.querySelector('#attempt')
 
 // Events
 
-buttonSend.addEventListener('click', newAttempt())
+buttonSend.addEventListener('click', newAttempt)
+buttonAgain.addEventListener('click', again)
 
+// Functions    
+function toggle(){
+    attemptBox.classList.toggle('hide')
+    responseBox.classList.toggle('hide')
+}
 
-// Functions 
+function again(){
+    toggle()
+}
 
-function newAttempt(){
-    attempt = document.querySelector('#attempt').value
-    randomNumber = (Math.random()*10).toFixed(0)
+function newAttempt(e){
+    e.preventDefault()
+    console.log(attemptNumber)
     
-    
-    if (attempt === randomNumber){
-        response = 'Parabéns, você acertou!!'
+    if (attempt.value === ''){
+        alert('Preencha a caixa de texto.')
+        
     } else{
-        response = 'Não foi dessa vez, tente novamente!!'
-    }
+        attemptNumber++
+        attempt.value
+        randomNumber = (Math.random()*10).toFixed(0)
+        
+        
+        if (attempt.value === randomNumber){            
+            response = `Parabéns, você acertou em ${attemptNumber} tentativas!!`
+            attemptNumber = 0
+            
+            toggle()
+            responseContent.innerText = response
+        } else{
+            attempt.value = ''
 
-    console.log(response)
+        }
+        console.log(attemptNumber, randomNumber)
+    }
 }
