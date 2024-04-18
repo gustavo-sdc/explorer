efc3df02ec7d22cbb4c84e71ed6dbbbd211b8bac
Gustavo Santos de Campos <92994147+Gustavo-SDC@users.noreply.github.com>
2024-04-18 03:58:12 -0300

    Development

--- modulo-08/new-node-project/src/server.js
+++ modulo-08/new-node-project/src/server.js
@@ -1,6 +1,27 @@
 const express = require('express');
 
 const app = express()
+app.use(express.json())
+
+app.get('/menssage/:id', (req, res)=>{
+    let { id } = req.params
+
+    res.send(`Mensagem ID: ${id}`)
+})
+
+app.get('/users', (req, res)=>{
+    let { page, user } = req.query
+
+    res.send(`Página: ${page}. Usuário: ${user}`)
+})
+
+app.post('/send', (req, res)=>{
+    let { user, email, password } = req.body 
+
+    //res.send(`Usuário (${user}). Email(${email}). Password(${password})`)
+    res.json({user, email, password})
+})
+
 const port = 3333;
 
 app.listen(port, ()=> console.log(`This server is running on Port ${port}`))
